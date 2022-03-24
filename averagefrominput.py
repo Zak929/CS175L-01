54dbd5403cb7297972fcd7b0eab296139c3ec718
Zak929 <91210754+Zak929@users.noreply.github.com>
2022-03-24 12:32:00 -0400

    Average from input with exceptions

--- averagefrominput.py
+++ averagefrominput.py
@@ -2,27 +2,28 @@
 #Zak Ahmed
 #CS-175L-01
 #Spring 2022
+import sys
 
 def main():
-    # Open a file for reading.
     count = 0
     total = 0
-    infile = open('numbers.txt', 'r')
-    for line in infile:
-        num = int(line)
-        count = count + 1
-        total = total + num
-        print(f'I read in {count} number(s) Current number is: \
-{num:.2f} Total is: {total:.2f}')
-    #Close the file.
-    infile.close()
+    try:
+        infile = open('numbers.txt', 'r')
+    except IOError:
+        print('There was an error trying to open the file')
+        sys.exit()
+    try:
+        for line in infile:
+            num = int(line)
+            count = count + 1
+            total = total + num
+            print(f'I read in {count} number(s) Current number is: \
+    {num:.2f} Total is: {total:.2f}')
+        infile.close()
+        average = total / count
+        print(f'Average: {average}')
+    except ValueError:
+        print('There was an error trying to read values inside the file')
 
-    # Add the numbers.
-    average = total / count
-
-    # Display the average.
-    print(f'Average: {average}')
-
-# Call the main function.
 if __name__ == '__main__':
     main()
